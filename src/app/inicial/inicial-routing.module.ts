de20d3784644a8ae5e1edb189974a584343bdd4c
Robson Alves <robsonalves87@live.com>
2023-01-22 17:24:02 -0300

    ajuste de rota

--- src/app/inicial/inicial-routing.module.ts
+++ src/app/inicial/inicial-routing.module.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import { InicialComponent } from './inicial.component';
 
 const routes: Routes = [
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
   { path: 'home', component: InicialComponent }
 ];
 
